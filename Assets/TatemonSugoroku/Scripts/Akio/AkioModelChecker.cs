3aba26a4e6fff08d691b53209155845a6fda84fb
AkioMabuchi <mabuchiakio@gmail.com>
2021-08-07 16:35:00 +0900

    FieldScoreを計算するメソッドの作成

--- Assets/TatemonSugoroku/Scripts/Akio/AkioModelChecker.cs
+++ Assets/TatemonSugoroku/Scripts/Akio/AkioModelChecker.cs
@@ -5,10 +5,47 @@ namespace TatemonSugoroku.Scripts.Akio
     public class AkioModelChecker : MonoBehaviour
     {
         private readonly FieldModel _fieldModel = new FieldModel();
-
+        
         void Start()
         {
+            FieldCell[] fieldCells = new FieldCell[64];
 
+            for (int i = 0; i < fieldCells.Length; i++)
+            {
+                fieldCells[i] = new FieldCell
+                {
+                    TatemonSpinPower = 0,
+                    TatemonPlayerId = -1,
+                    DomainPlayerId = -1
+                };
+            }
+
+            fieldCells[0] = new FieldCell
+            {
+                TatemonSpinPower = 1,
+                TatemonPlayerId = 0,
+                DomainPlayerId = 0
+            };
+            fieldCells[1] = new FieldCell
+            {
+                TatemonSpinPower = 2,
+                TatemonPlayerId = 0,
+                DomainPlayerId = 0
+            };
+            fieldCells[2] = new FieldCell
+            {
+                TatemonSpinPower = 0,
+                TatemonPlayerId = -1,
+                DomainPlayerId = 0
+            };
+            fieldCells[3] = new FieldCell
+            {
+                TatemonSpinPower = 3,
+                TatemonPlayerId = 0,
+                DomainPlayerId = 0
+            };
+
+            Debug.Log("FIELD SCORE: " + ScoreModel.CalculateFieldScore(fieldCells, 0));
         }
 
         // Update is called once per frame
--- Assets/TatemonSugoroku/Scripts/Akio/ScoreModel.cs
+++ Assets/TatemonSugoroku/Scripts/Akio/ScoreModel.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace TatemonSugoroku.Scripts.Akio
@@ -15,6 +16,94 @@ namespace TatemonSugoroku.Scripts.Akio
         public void Dispose()
         {
             
+        }
+
+        // FieldCellsとPlayerIdを入力して、盤面上のスコアを返す静的メソッドです。
+        public static int CalculateFieldScore(FieldCell[] fieldCells, int playerId)
+        {
+            List<List<int>> graph = new List<List<int>>();
+            List<bool> seen = new List<bool>();
+
+            for (int i = 0; i < fieldCells.Length; i++)
+            {
+                graph.Add(new List<int>());
+                seen.Add(false);
+            }
+
+            for (int i = 0; i < fieldCells.Length; i++)
+            {
+                if (fieldCells[i].DomainPlayerId == playerId)
+                {
+                    if (i % 8 < 7)
+                    {
+                        int j = i + 1;
+                        if (fieldCells[i].DomainPlayerId == fieldCells[j].DomainPlayerId)
+                        {
+                            graph[i].Add(j);
+                            graph[j].Add(i);
+                        }
+                    }
+
+                    if (i / 8 < 7)
+                    {
+                        int j = i + 8;
+                        if (fieldCells[i].DomainPlayerId == fieldCells[j].DomainPlayerId)
+                        {
+                            graph[i].Add(j);
+                            graph[j].Add(i);
+                        }
+                    }
+                }
+                else
+                {
+                    seen[i] = true;
+                }
+            }
+
+            int r = 0;
+            for (int i = 0; i < fieldCells.Length; i++)
+            {
+                if (seen[i])
+                {
+                    continue;
+                }
+
+                HashSet<int> indexes = new HashSet<int>();
+                Queue<int> queue = new Queue<int>();
+                seen[i] = true;
+                queue.Enqueue(i);
+                indexes.Add(i);
+                
+                while (queue.Count > 0)
+                {
+                    int v = queue.Dequeue();
+                    foreach (int nv in graph[v])
+                    {
+                        if (seen[nv])
+                        {
+                            continue;
+                        }
+
+                        seen[nv] = true;
+                        indexes.Add(nv);
+                        queue.Enqueue(nv);
+                    }
+                }
+
+                int r1 = indexes.Count;
+                int r2 = 0;
+                foreach (int index in indexes)
+                {
+                    if (fieldCells[index].TatemonPlayerId == playerId)
+                    {
+                        r2 += fieldCells[index].TatemonSpinPower;
+                    }
+                }
+
+                r += r1 * r2;
+            }
+
+            return r;
         }
     }
 }
